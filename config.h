7ed68f4537efbd2c261eca81b574bf58d9905e0b
KazanDzibre <markoruzic997@gmail.com>
2020-06-22 12:31:27 +0200

    rade emoji novi

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=8", "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "monospace:size=8", "EmojiOne:size=8:antialias=true:autohint=true"  };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
